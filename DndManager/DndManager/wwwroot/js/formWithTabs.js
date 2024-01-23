5045a5ef294205ec27b5ad8734d9441dc358eaf5
3elk4 <izaa1109@gmail.com>
2024-01-23 22:14:03 +0100

    change some css and js

--- DndManager/DndManager/wwwroot/js/formWithTabs.js
+++ DndManager/DndManager/wwwroot/js/formWithTabs.js
@@ -12,9 +12,9 @@ function showTab(n) {
         document.getElementById("prevBtn").style.display = "inline";
     }
     if (n == (x.length - 1)) {
-        document.getElementById("nextBtn").innerHTML = "Submit";
+        document.getElementById("nextBtn").innerHTML = "<i class='fa'>&#xf00c</i>";
     } else {
-        document.getElementById("nextBtn").innerHTML = "Next";
+        document.getElementById("nextBtn").innerHTML = "<i class='fa'>&#xf0da</i>";
     }
     // ... and run a function that displays the correct step indicator:
     fixStepIndicator(n)
