d63b047f767e5416cb7fb3f75f426f98f7e188c1
3elk4 <izaa1109@gmail.com>
2024-02-12 21:25:27 +0100

    remove unused commands and queries

--- DndManager/Application/Bio/Commands/Update/UpdateBioCommandValidator.cs
+++ DndManager/Application/Bio/Commands/Update/UpdateBioCommandValidator.cs
@@ -9,23 +9,23 @@ namespace Application.Bio.Commands.Update
 {
     class UpdateBioCommandValidator : AbstractValidator<UpdateBioCommand>
     {
-        public UpdateBioCommandValidator()
-        {
-            RuleFor(v => v.Age).GreaterThan(0);
-            RuleFor(v => v.Size).MaximumLength(45);
-            RuleFor(v => v.Weight).MaximumLength(45);
-            RuleFor(v => v.Height).MaximumLength(45);
-            RuleFor(v => v.Skin).MaximumLength(45);
-            RuleFor(v => v.Eyes).MaximumLength(45);
-            RuleFor(v => v.Hair).MaximumLength(45);
-            RuleFor(v => v.Alignment).MaximumLength(45);
-            RuleFor(v => v.Traits).MaximumLength(500);
-            RuleFor(v => v.Flaws).MaximumLength(500);
-            RuleFor(v => v.Bonds).MaximumLength(500);
-            RuleFor(v => v.Ideals).MaximumLength(500);
-            RuleFor(v => v.Allies).MaximumLength(500);
-            RuleFor(v => v.Backstory).MaximumLength(1000);
-            RuleFor(v => v.Id).NotEmpty();
-        }
+        //public UpdateBioCommandValidator()
+        //{
+        //    RuleFor(v => v.Age).GreaterThan(0);
+        //    RuleFor(v => v.Size).MaximumLength(45);
+        //    RuleFor(v => v.Weight).MaximumLength(45);
+        //    RuleFor(v => v.Height).MaximumLength(45);
+        //    RuleFor(v => v.Skin).MaximumLength(45);
+        //    RuleFor(v => v.Eyes).MaximumLength(45);
+        //    RuleFor(v => v.Hair).MaximumLength(45);
+        //    RuleFor(v => v.Alignment).MaximumLength(45);
+        //    RuleFor(v => v.Traits).MaximumLength(500);
+        //    RuleFor(v => v.Flaws).MaximumLength(500);
+        //    RuleFor(v => v.Bonds).MaximumLength(500);
+        //    RuleFor(v => v.Ideals).MaximumLength(500);
+        //    RuleFor(v => v.Allies).MaximumLength(500);
+        //    RuleFor(v => v.Backstory).MaximumLength(1000);
+        //    RuleFor(v => v.Id).NotEmpty();
+        //}
     }
 }
