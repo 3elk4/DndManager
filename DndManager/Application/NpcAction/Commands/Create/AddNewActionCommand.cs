a68e777bd8e404e1d7dbb1e0257ef554e565bdaa
3elk4 <izaa1109@gmail.com>
2024-02-19 14:05:46 +0100

    add npc action commands and queries

---
+++ DndManager/Application/NpcAction/Commands/Create/AddNewActionCommand.cs
@@ -0,0 +1,62 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using AutoMapper;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.NpcAction.Commands.Create
+{
+    public record AddNewActionCommand : IRequest<Result<NpcActionVM>>, ICommand
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public NpcAttackVM Attack { get; set; }
+        public NpcDamageVM Damage { get; set; }
+        public string NpcId { get; set; }
+    }
+
+    public class AddNewCombatActionCommandHandler : IRequestHandler<AddNewActionCommand, Result<NpcActionVM>>
+    {
+        private readonly IDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public AddNewCombatActionCommandHandler(IDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<NpcActionVM>> Handle(AddNewActionCommand request, CancellationToken cancellationToken)
+        {
+            var combatAction = new Domain.Entities.NpcAction()
+            {
+                Name = request.Name,
+                Type = request.Type,
+                Description = request.Description,
+                NpcId = request.NpcId,
+                Attack = new Domain.Entities.NpcAttack()
+                {
+                    ToHit = request.Attack.ToHit,
+                    Type = request.Attack.Type,
+                    Target = request.Attack.Target,
+                    Range = request.Attack.Range
+                },
+                Damage = new Domain.Entities.NpcDamage()
+                {
+                    DamageDice = request.Damage.DamageDice,
+                    DamageType = request.Damage.DamageType,
+                }
+            };
+
+            _dbContext.NpcActions.Add(combatAction);
+            var result = await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return result > 0 ?
+                Result<NpcActionVM>.Success(_mapper.Map<NpcActionVM>(combatAction)) :
+                Result<NpcActionVM>.Failure(null, new List<string>() { "Some problems occured during creating record." });
+        }
+    }
+}
---
+++ DndManager/Application/NpcAction/NpcAttackVM.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common.Models;
+using AutoMapper;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.NpcAction
+{
+    public class NpcAttackVM : BaseVM
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please provide action to hit")]
+        [Range(0, 5000, ErrorMessage = "Attack additonal bonus should be min 0 and max 5000")]
+        public int ToHit { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please provide action type")]
+        [MaxLength(50, ErrorMessage = "Attack type should be min 1 and max 50 length")]
+        public string Type { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please provide action target")]
+        [MaxLength(50, ErrorMessage = "Attack target should be min 1 and max 50 length")]
+        public string Target { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please provide action range")]
+        [MaxLength(50, ErrorMessage = "Attack range should be min 1 and max 50 length")]
+        public string Range { get; set; }
+
+        public string ActionId { get; set; }
+        private class Mapping : Profile
+        {
+            public Mapping()
+            {
+                CreateMap<Domain.Entities.NpcAttack, NpcAttackVM>();
+                CreateMap<NpcAttackVM, Domain.Entities.NpcAttack>();
+            }
+        }
+    }
+}
