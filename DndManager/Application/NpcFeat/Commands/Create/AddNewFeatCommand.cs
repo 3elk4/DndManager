273bb44f8a6091d0954455b0a685c2f3a8863ca5
3elk4 <izaa1109@gmail.com>
2024-02-19 14:05:14 +0100

    add feat commands and queries

---
+++ DndManager/Application/NpcFeat/Commands/Create/AddNewFeatCommand.cs
@@ -0,0 +1,48 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using AutoMapper;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.NpcFeat.Commands.Create
+{
+    public record AddNewFeatCommand : IRequest<Result<NpcFeatVM>>, ICommand
+    {
+        public string Name { get; set; }
+        public string TimeRegeneration { get; set; }
+        public string Description { get; set; }
+        public string NpcId { get; set; }
+    }
+
+    public class AddNewFeatCommandHandler : IRequestHandler<AddNewFeatCommand, Result<NpcFeatVM>>
+    {
+        private readonly IDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public AddNewFeatCommandHandler(IDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<NpcFeatVM>> Handle(AddNewFeatCommand request, CancellationToken cancellationToken)
+        {
+            var item = new Domain.Entities.NpcFeat()
+            {
+                Name = request.Name,
+                TimeRegeneration = request.TimeRegeneration,
+                Description = request.Description,
+                NpcId = request.NpcId
+            };
+
+            _dbContext.NpcFeats.Add(item);
+            var result = await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return result == 1 ?
+                Result<NpcFeatVM>.Success(_mapper.Map<NpcFeatVM>(item)) :
+                Result<NpcFeatVM>.Failure(null, new List<string>() { "Some problems occured during creating record." });
+        }
+    }
+}
---
+++ DndManager/Application/NpcFeat/Commands/Update/UpdateFeatCommand.cs
@@ -0,0 +1,48 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using AutoMapper;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.NpcFeat.Commands.Update
+{
+    public record UpdateFeatCommand : IRequest<Result<int>>, ICommand
+    {
+        public string Name { get; set; }
+        public string TimeRegeneration { get; set; }
+        public string Description { get; set; }
+        public string Id { get; set; }
+    }
+
+
+    public class UpdateFeatCommandHandler : IRequestHandler<UpdateFeatCommand, Result<int>>
+    {
+        private readonly IDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public UpdateFeatCommandHandler(IDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<int>> Handle(UpdateFeatCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.NpcFeats.FindAsync(new object[] { request.Id }, cancellationToken);
+
+            if (entity == null) Result<int>.Failure(0, new List<string>() { $"Can't find an item with id {request.Id}." });
+
+            entity.Name = request.Name;
+            entity.TimeRegeneration = request.TimeRegeneration;
+            entity.Description = request.Description;
+
+            var result = await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return result > 0 ?
+                   Result<int>.Success(result) :
+                   Result<int>.Failure(0, new List<string>() { "Some errors occured during updating records." });
+        }
+    }
+}
