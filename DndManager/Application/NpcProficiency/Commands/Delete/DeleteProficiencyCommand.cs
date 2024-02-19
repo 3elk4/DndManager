3edf6694a70e58cd39e2f3d83209f1d24b989451
3elk4 <izaa1109@gmail.com>
2024-02-19 14:05:29 +0100

    add npc proficiency commands and queries

---
+++ DndManager/Application/NpcProficiency/Commands/Delete/DeleteProficiencyCommand.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.NpcProficiency.Commands.Delete
+{
+    public record DeleteProficiencyCommand : IRequest<Result>, ICommand
+    {
+        public string Id { get; init; }
+    }
+
+    public class DeleteProficiencyCommandHandler : IRequestHandler<DeleteProficiencyCommand, Result>
+    {
+        private readonly IDbContext _dbContext;
+
+        public DeleteProficiencyCommandHandler(IDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Result> Handle(DeleteProficiencyCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.NpcProficiencies.FindAsync(new object[] { request.Id }, cancellationToken);
+
+            _dbContext.NpcProficiencies.Remove(entity);
+            var result = await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return result == 1 ? Result.Success() : Result.Failure(new List<string>() { "Some errors occured during deleting record" });
+        }
+    }
+}
---
+++ DndManager/Application/NpcProficiency/Queries/Index/GetManyProficienciesByNpcIdQuery.cs
@@ -0,0 +1,42 @@
+﻿using Application.Common.Extentions;
+using Application.Common.Interfaces;
+using Application.Common.Models;
+using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.NpcProficiency.Queries.Index
+{
+    public record GetManyProficienciesByNpcIdQuery : IRequest<Result<List<NpcProficiencyVM>>>, IQuery
+    {
+        public string NpcId { get; init; }
+    }
+
+    public class GetManyFeatsByNpcIdQueryHandler : IRequestHandler<GetManyProficienciesByNpcIdQuery, Result<List<NpcProficiencyVM>>>, IQuery
+    {
+        private readonly IDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetManyFeatsByNpcIdQueryHandler(IDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<List<NpcProficiencyVM>>> Handle(GetManyProficienciesByNpcIdQuery request, CancellationToken cancellationToken)
+        {
+            return Result<List<NpcProficiencyVM>>.Success(
+                await _dbContext
+                    .NpcProficiencies
+                    .Where(item => item.NpcId.Equals(request.NpcId))
+                    .ProjectToListAsync<NpcProficiencyVM>(_mapper.ConfigurationProvider)
+                );
+        }
+
+    }
+}
