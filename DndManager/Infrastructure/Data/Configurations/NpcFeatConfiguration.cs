a4476f6534bd825cdd00799c0469777266394e45
3elk4 <izaa1109@gmail.com>
2024-02-19 14:04:43 +0100

    add feat and proficiency configuration

---
+++ DndManager/Infrastructure/Data/Configurations/NpcFeatConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations
+{
+    class NpcFeatConfiguration : BaseEntityConfiguration<NpcFeat>
+    {
+        public override void Configure(EntityTypeBuilder<NpcFeat> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(a => a.Name).HasMaxLength(100).IsRequired();
+            builder.Property(a => a.Description).HasMaxLength(1000).IsRequired();
+            builder.Property(a => a.TimeRegeneration).HasMaxLength(100);
+
+            builder.HasOne(a => a.Npc).WithMany(x => x.Feats).HasForeignKey(x => x.NpcId).IsRequired().OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ DndManager/Infrastructure/Data/Configurations/NpcProficiencyConfiguraion.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Configurations
+{
+    class NpcProficiencyConfiguraion : BaseEntityConfiguration<NpcProficiency>
+    {
+        public override void Configure(EntityTypeBuilder<NpcProficiency> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(a => a.Name).HasMaxLength(100).IsRequired();
+            builder.Property(a => a.Type).HasMaxLength(100).IsRequired();
+            builder.Property(a => a.Range).HasMaxLength(50);
+
+            builder.HasOne(a => a.Npc).WithMany(x => x.Proficiencies).HasForeignKey(x => x.NpcId).IsRequired().OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
