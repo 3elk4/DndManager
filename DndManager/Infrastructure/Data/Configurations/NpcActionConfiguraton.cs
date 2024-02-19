402d9ffb53f619530adab2313f45823e4ebc0528
3elk4 <izaa1109@gmail.com>
2024-02-19 14:04:55 +0100

    add npc action configuration

---
+++ DndManager/Infrastructure/Data/Configurations/NpcActionConfiguraton.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations
+{
+    class NpcActionConfiguraton : BaseEntityConfiguration<NpcAction>
+    {
+        public override void Configure(EntityTypeBuilder<NpcAction> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(a => a.Name).HasMaxLength(100).IsRequired();
+            builder.Property(a => a.Type).HasMaxLength(100).IsRequired();
+            builder.Property(a => a.Description).HasMaxLength(500);
+
+            builder.HasOne(a => a.Npc).WithMany(x => x.Actions).HasForeignKey(x => x.NpcId).IsRequired().OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
