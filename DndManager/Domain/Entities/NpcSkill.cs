6d5f1976a16b02122a6242293c0800309ee5382e
3elk4 <izaa1109@gmail.com>
2024-04-17 15:00:37 +0200

    make some ints nullable + proficiency name longer

--- DndManager/Domain/Entities/NpcSkill.cs
+++ DndManager/Domain/Entities/NpcSkill.cs
@@ -3,7 +3,7 @@
     public class NpcSkill : BaseAuditableEntity
     {
         public string Name { get; set; }
-        public int Bonus { get; set; }
+        public int? Bonus { get; set; } = null;
 
         public string NpcAbilityId { get; set; }
         public NpcAbility NpcAbility { get; set; } = null!;
